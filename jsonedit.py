dc9a0664dd1cdb06f03e37c47e3b3bd9d3e5b8c6
Qian Zhou <qzhou@stuy.edu>
2019-04-05 13:16:22 -0400

    try this link with gini, and weed_price changed to canabis_price

--- jsonedit.py
+++ jsonedit.py
@@ -7,6 +7,12 @@ fContent= f.read()
 f.close()
 d=json.loads(fContent)
 
+#reads gini coefficient by state
+g = open('gini.json','r')
+gcontent = g.read()
+g.close()
+gini=json.loads(gcontent)
+
 #get states information from us.json
 states=d['objects']['usStates']['geometries']
 
@@ -37,13 +43,22 @@ for line in canaPfileLines:
 #print(cana.keys())
 #print(cana['"Hawaii"'])
 
+
+
 #provide content to new us.json
 for state in states:
     stNM='"'+abbrNm[state['properties']['STATE_ABBR']]+'"'
     if stNM != '"District Of Columbia"':
-        state["properties"]["WEED_PRICE"]=float(cana[stNM][0][1:-1])
+        state["properties"]["CANABIS_PRICE"]=float(cana[stNM][0][1:-1])
+        try:
+            del state["properties"]["WEED_PRICE"]
+        except:
+            print('')
         #------------------------may add more information here------------------
 
+        #----------------gini coefficient --------------
+        stNM = stNM [1:-1]
+        state["properties"]["GINI"]=float(gini[stNM+' '])
 
 #print(d)
 s= str(d).replace("'",'"')
